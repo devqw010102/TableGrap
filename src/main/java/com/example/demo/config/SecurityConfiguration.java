25ebe4208ac712884ec4109dec4c8d1e3a253af7
devqw010102 <qw010102@gmail.com>
2025-12-12 09:15:34 +0900

    modify: security

--- src/main/java/com/example/demo/config/SecurityConfiguration.java
+++ src/main/java/com/example/demo/config/SecurityConfiguration.java
@@ -3,6 +3,8 @@ package com.example.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -30,5 +32,10 @@ public class SecurityConfiguration {
                         .permitAll());
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
