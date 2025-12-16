2914792126df3ce3bed82a929e8c7531ae11fa1d
JJ LEE <jjtw8711@gmail.com>
2025-12-16 17:29:33 +0900

    addfunction/reservation edit and cancel

--- src/main/java/com/example/demo/data/dto/BookResponseDto.java
+++ src/main/java/com/example/demo/data/dto/BookResponseDto.java
@@ -1,5 +1,6 @@
 package com.example.demo.data.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,7 +13,12 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class BookResponseDto {
 
+    private Long bookId;    //예약 수정용
+    private Long dinerId;   //예약 수정용
+
     private String dinerName;
+
+    // @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
     private LocalDateTime bookingDate;
     private Integer personnel;
     private String memberName;
--- src/main/java/com/example/demo/service/impl/BookServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/BookServiceImpl.java
@@ -1,10 +1,11 @@
 package com.example.demo.service.impl;
 
+import com.example.demo.data.dto.BookDto;
 import com.example.demo.data.dto.BookResponseDto;
 import com.example.demo.data.model.Book;
-import com.example.demo.data.model.Member;
 import com.example.demo.data.repository.BookRepository;
 import com.example.demo.service.BookService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -21,6 +22,8 @@ public class BookServiceImpl implements BookService {
         return bookRepository.findByMember_id(memberId)
                 .stream()
                 .map(book -> BookResponseDto.builder()
+                        .bookId(book.getBookId())   //  bookresponse dto에
+                        .dinerId(book.getDiner().getId()) //  bookresponse dto에
                         .dinerName(book.getDiner().getDinerName())
                         .bookingDate(book.getBookingDate())
                         .personnel(book.getPersonnel())
@@ -28,5 +31,36 @@ public class BookServiceImpl implements BookService {
                         .success(book.getSuccess())
                         .build())
                 .toList();
+    }
+
+    @Override
+    public BookResponseDto getBooking(Long bookId) {
+        Book book = bookRepository.findById(bookId)
+                .orElseThrow(() -> new IllegalArgumentException("예약이 존재하지 않습니다."));
+
+        // Entity -> DTO 변환
+        return BookResponseDto.builder()
+                .bookId(book.getBookId())
+                .dinerId(book.getDiner().getId())
+                .dinerName(book.getDiner().getDinerName())
+                .bookingDate(book.getBookingDate())
+                .personnel(book.getPersonnel())
+                .build();
+    }
+
+    @Override
+    @Transactional
+    public void updateBooking(BookDto dto) {
+        Book book = bookRepository.findById(dto.getBookId())
+                .orElseThrow(() -> new IllegalArgumentException("예약이 존재하지 않습니다."));
+
+        //update
+        book.setBookingDate(dto.getBookingDate());
+        book.setPersonnel(dto.getPersonnel());
+    }
+
+    @Override
+    public void deleteBooking(Long bookId) {
+        bookRepository.deleteById(bookId);
     }
 }
